711646586d47931cbff0863768e3c5089c2f3967
Brian Phan <b.phan@berkeley.edu>
2022-04-05 23:05:21 -0700

    Add some css

--- src/App.js
+++ src/App.js
@@ -5,16 +5,23 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Brian Phan</h1>
-        <p>SWE @ Amazon, Snackpass</p>
-        <p>UC Berkeley Sophomore studying computer science.</p>
-        <p>
-          Maker of stuff. I cook the best teriyaki chicken in the game, and
-          create the most fire copyright infringing Cal merch.
-        </p>
-        <p>I’m also really good at making apps.</p>
+        <h1 className="GOAT">Brian Phan</h1>
+        <span className="clout">
+          SWE @ <span className="bezos-600">Amazon,</span>{" "}
+          <span className="tochi-600">Snackpass</span>
+        </span>
+        <div>
+          <p className="soquirky">
+            UC Berkeley Sophomore studying computer science.
+          </p>
+          <p className="soquirky">
+            Maker of stuff. I cook the best teriyaki chicken in the game, and
+            create the most fire copyright infringing Cal merch.
+          </p>
+          <p className="soquirky">I’m also really good at making apps.</p>
+        </div>
         {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p>   
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -27,14 +34,14 @@ function App() {
         </a> */}
       </header>
       <main>
-        <h2>Experience</h2>
-        <h3>Snackpass</h3>
-        <p>
+        <h2 className="H-E-A-D-E-R">Experience</h2>
+        <h3 className="namerecognition">Snackpass</h3>
+        <p className="corporateslavery">
           Worked on new accounting system (order management, restaurant
           reporting, CI/CD, prod/staging environments) that now processes $60M+
           a year.
         </p>
-        <h2>Projects</h2>
+        <h2 className="H-E-A-D-E-R">Projects</h2>
       </main>
     </div>
   );
