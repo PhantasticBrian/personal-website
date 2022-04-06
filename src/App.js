8d44a7851bf59c24f6b0a204001d9b29e5e32e47
Brian Phan <b.phan@berkeley.edu>
2022-04-05 22:06:19 -0700

    Add content

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Brian Phan</h1>
+        <p>SWE @ Amazon, Snackpass</p>
+        <p>UC Berkeley Sophomore studying computer science.</p>
+        <p>
+          Maker of stuff. I cook the best teriyaki chicken in the game, and
+          create the most fire copyright infringing Cal merch.
+        </p>
+        <p>I’m also really good at making apps.</p>
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,8 +24,18 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
+      <main>
+        <h2>Experience</h2>
+        <h3>Snackpass</h3>
+        <p>
+          Worked on new accounting system (order management, restaurant
+          reporting, CI/CD, prod/staging environments) that now processes $60M+
+          a year.
+        </p>
+        <h2>Projects</h2>
+      </main>
     </div>
   );
 }
